9b4f77bb3de68f5cd46caa96822cac48dafb0f03
Jagan <jaganelavarasan@gmail.com>
2016-09-02 23:24:33 +0530

    Create Pair.java

---
+++ Map/Pair.java
@@ -0,0 +1,37 @@
+package org.creators;
+import java.io.IOException;
+import java.util.*;
+public class ModifiedMap {
+private static Scanner sc;
+
+public static void main(String args[]) throws IOException {
+
+	Map<String,Pair> mp=new TreeMap<String,Pair>();
+	sc = new Scanner(System.in);
+	int n;
+	n=sc.nextInt();
+	while(n>0){
+		String s;
+		Double f;
+		s=sc.next();
+		f=sc.nextDouble();
+		if(mp.containsKey(s)){
+			Pair p=mp.get(s);
+			p.setKey1((Double)p.getKey1()+f);
+			p.setKey2((Integer)p.getKey2()+1);
+			mp.put(s,p);
+		}else{
+			
+			Pair p=new Pair();
+			p.setKey1(f);
+			p.setKey2(1);
+			mp.put(s,p);
+		}
+		n-=1;
+		
+	}
+	for(Map.Entry<String,Pair> each: mp.entrySet()){
+		System.out.println(each.getKey()+" "+each.getValue());
+	}
+}
+}
